e12169dbd1db3cf0fa920bfb9d5e1d6183343849
CC <cc@battab.com>
2022-10-06 22:25:59 -0600

    Get basic card layout settled, displaying only pictures

--- resources/views/pages/pokemonset.blade.php
+++ resources/views/pages/pokemonset.blade.php
@@ -27,35 +27,31 @@ ccmakesthings
     grid-gap: 1rem;
 }
 
-.card {
-    background-color: lightgray;
-    border: 2px solid #e7e7e7;
-    border-radius: 4px; 
-    padding: .5rem;
+.have {
+
 }
 
-.cardIHave {
-    background-color: white;
-    border: 2px solid #e7e7e7;
-    border-radius: 4px; 
-    padding: .5rem;
+.dont-have {
+    opacity: 0.4;
 }
 
+.card-image {
+    max-width:100%;
+    height:auto;
+}
 
 </style>
 	<div class="container">
 <h2><a href="/pokemon">Sets</a> > {{$set->name}}</h2>
 <div class="cards">
     @foreach ($cards as $card)
-        @if ($card->user_id)
-        <div class="cardIHave">
-        @else
-        <div class="card">
-        @endif
-            <a href="">{{$card->name}}</a><br>
-            <img src=""><br>
-            {{$card->rarity_id}}
-
+        <div>
+            <a href="">{{$card->name}}</a> {{$card->set_number}}<br>
+            @if ($card->user_id)
+            <img class="card-image" src="/images/pokemon/{{$set->url}}/{{$card->set_number}}.jpg"><br>
+            @else
+            <img class="card-image dont-have" src="/images/pokemon/{{$set->url}}/{{$card->set_number}}.jpg"><br>
+            @endif
         </div>
     @endforeach
 </div>
