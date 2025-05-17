956bdb65cfa2f10488fb068dcfd4d2f7ca188524
CC <cc@battab.com>
2025-05-17 00:28:33 -0600

    it wasn't different

--- resources/views/pages/onepieceneed.blade.php
+++ resources/views/pages/onepieceneed.blade.php
@@ -59,7 +59,6 @@ ccmakesthings
                 @else
                 <img class="card-image" src="/images/onepiece/{{$set->url}}/{{$set->imagenumber()}}-{{$card->set_number}}-{{$card->card_number}}.png"><br>
                 @endif
-                {{ dd($card)}}
                 {{ $card->last_price}}
                 <br>
             </div>
