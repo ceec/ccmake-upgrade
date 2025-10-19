df2d8c5666a7ce3cd4e27a410508f41b975cbeea
CC <cc@battab.com>
2025-10-19 00:18:39 -0600

    Link to tcgplayer

--- resources/views/pages/pokemonCard.blade.php
+++ resources/views/pages/pokemonCard.blade.php
@@ -29,7 +29,7 @@ ccmakesthings
             <h2>{{$card->name}}</h2>
             Set: <a href="/pokemon/set/{{$set->url}}/#{{$card->id}}">{{$set->name}}</a><br><br>
             <hr>
-            tcgcsv Id: {{$card->tcgcsv_id}}<br>
+            tcgcsv Id: <a href="https://www.tcgplayer.com/product/{{$card->tcgcsv_id}}?Language=English">{{$card->tcgcsv_id}}</a><br>
             Latest Price: {{$card->lastPrice()}}<br>
             Price history:<br>
             @foreach($prices as $price)
