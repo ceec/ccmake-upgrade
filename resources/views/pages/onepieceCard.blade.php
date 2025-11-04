e62f8dd3f39a93ce56067ffe761217d47d021a4a
CC <cc@battab.com>
2025-11-03 21:30:49 -0700

    Add price graph

--- resources/views/pages/onepieceCard.blade.php
+++ resources/views/pages/onepieceCard.blade.php
@@ -37,7 +37,108 @@ ccmakesthings
             @foreach($prices as $price)
                 {{$price->created_at}} - ${{$price->price}}<br>
             @endforeach
+            <style>
+                #chartdiv {
+                width: 100%;
+                height: 300px;
+                max-width: 100%;
+                }
+            </style>
+            <!-- Resources -->
+            <script src="https://cdn.amcharts.com/lib/5/index.js"></script>
+            <script src="https://cdn.amcharts.com/lib/5/xy.js"></script>
+            <script src="https://cdn.amcharts.com/lib/5/themes/Animated.js"></script>
+            <!-- Chart code -->
+            <script>
+            am5.ready(function() {
 
+            // Create root element
+            // https://www.amcharts.com/docs/v5/getting-started/#Root_element
+            var root = am5.Root.new("chartdiv");
+
+            // Set themes
+            // https://www.amcharts.com/docs/v5/concepts/themes/
+            root.setThemes([
+            am5themes_Animated.new(root)
+            ]);
+
+            // Create chart
+            // https://www.amcharts.com/docs/v5/charts/xy-chart/
+            var chart = root.container.children.push(am5xy.XYChart.new(root, {
+            panX: true,
+            panY: true,
+            wheelX: "panX",
+            wheelY: "zoomX",
+            pinchZoomX:true,
+            paddingLeft: 0
+            }));
+
+            // Add cursor
+            // https://www.amcharts.com/docs/v5/charts/xy-chart/cursor/
+            var cursor = chart.set("cursor", am5xy.XYCursor.new(root, {
+            behavior: "none"
+            }));
+            cursor.lineY.set("visible", false);
+
+
+            // Create axes
+            // https://www.amcharts.com/docs/v5/charts/xy-chart/axes/
+            var xAxis = chart.xAxes.push(am5xy.DateAxis.new(root, {
+            maxDeviation: 0.2,
+            baseInterval: {
+                timeUnit: "day",
+                count: 1
+            },
+            renderer: am5xy.AxisRendererX.new(root, {
+                minorGridEnabled:true
+            }),
+            tooltip: am5.Tooltip.new(root, {})
+            }));
+
+            var yAxis = chart.yAxes.push(am5xy.ValueAxis.new(root, {
+            renderer: am5xy.AxisRendererY.new(root, {
+                pan:"zoom"
+            })  
+            }));
+
+
+            // Add series
+            // https://www.amcharts.com/docs/v5/charts/xy-chart/series/
+            var series = chart.series.push(am5xy.LineSeries.new(root, {
+            name: "Series",
+            xAxis: xAxis,
+            yAxis: yAxis,
+            valueYField: "value",
+            valueXField: "date",
+            tooltip: am5.Tooltip.new(root, {
+                labelText: "{valueY}"
+            })
+            }));
+
+            var test = [];
+            @foreach($prices as $price)
+                test.push({"date":"{{$price->created_at}}","value":{{$price->price}}});
+            @endforeach
+
+            // Creating processor only for the first series
+            series.data.processor = am5.DataProcessor.new(root, {
+                dateFields: ["date"],
+            dateFormat: "yyyy-MM-dd H:m:s"
+            });
+
+
+            series.data.setAll(test);
+
+            // Make stuff animate on load
+            // https://www.amcharts.com/docs/v5/concepts/animations/
+            series.appear(1000);
+            chart.appear(1000, 100);
+
+            }); // end am5.ready()
+</script>
+
+<!-- HTML -->
+<div id="chartdiv"></div>
             @if (!Auth::guest())
                 <br><a href="/dashboard/onepiececard/edit/{{$card->id}}">Edit Card</a><br><br>
                 @if (Auth::user()->id == 1)
