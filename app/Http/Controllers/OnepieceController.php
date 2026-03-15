2c3100da4c1d7dac75f0e8a9237ee91023f01595
CC <cc@battab.com>
2026-03-15 13:14:16 -0600

    Limit the sets to ones that have new original cards

--- app/Http/Controllers/OnepieceController.php
+++ app/Http/Controllers/OnepieceController.php
@@ -189,7 +189,11 @@ class OnepieceController extends Controller
      * @return \Illuminate\Http\Response
      */
     public function addCardDisplay() {
-        $sets = Onepieceset::orderBy('shortname','ASC')->pluck('shortname','id');
+        $sets = Onepieceset::where('shortname','LIKE','OP-%')
+                ->orWhere('shortname','LIKE','ST-%')
+                ->orWhere('shortname','LIKE','EB-%')
+                ->orWhere('shortname','LIKE','PRB-%')
+                ->orderBy('shortname','ASC')->pluck('shortname','id');
         $characters = Onepiececharacter::orderBy('name','ASC')->pluck('name','id');
         // last card
         $last = Onepiececard::orderBy('created_at','DESC')->pluck('set_number')->first();
